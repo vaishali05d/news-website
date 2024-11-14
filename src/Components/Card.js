44459a28ae682044f4562da74b7b4c58242bb560
vaishali05d <sharma.vaishali.0512@gmail.com>
2024-11-14 20:47:59 +0530

    first commit

---
+++ src/Components/Card.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Card = ({ data }) => {
+    console.log(data);
+
+    return (
+        <div className='cardContainer'>
+            {data.map((curItem,index)=>{
+                 if(!curItem.urlToImage){
+                    return null
+                }else{
+                    return(
+                        <div className='card'>
+                            <img src={curItem.urlToImage}/>
+                            <div className='content'>
+                                <a className='title' onClick={()=>window.open(curItem.url)}>{curItem.title}</a>
+                                <p>{curItem.description}</p>
+                                <button onClick={()=>window.open(curItem.url)}>Read More</button>
+                            </div>
+                        </div>
+                    )
+                }
+                
+            })}
+        </div>
+    )
+}
+
+export default Card
---
+++ src/Components/NewsApp.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import Card from './Card';
+
+const NewsApp = () => {
+    const [search, setSearch] = useState("india");
+    const [newsData, setNewsData] = useState(null);
+    const API_KEY = "38d7b78ca16c4ada91b599dd6a4af2dd";
+
+    const getData = async () => {
+        const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
+        const jsonData = await response.json();
+        console.log(jsonData.articles);
+        setNewsData(jsonData.articles);
+
+    }
+    useEffect(()=>{
+        getData()
+    },[])
+
+    const handleInput = (e) => {
+        console.log(e.target.value);
+        setSearch(e.target.value)
+
+    }
+    const userInput = (event) =>{
+        setSearch(event.target.value)
+    }
+    return (
+        <div>
+            <nav>
+                <div>
+                    <h1>Trendy News</h1>
+                </div>
+                <ul style={{ display: "flex", gap: "11px" }}>
+                    <a style={{ fontWeight: 600, fontSize: "17px" }}>All News</a>
+                    <a style={{ fontWeight: 600, fontSize: "17px" }}>Trending</a>
+
+                </ul>
+                <div className='searchBar'>
+                    <input type='text' placeholder='Search News' value={search} onChange={handleInput} />
+                    <button onClick={getData}>Search</button>
+                </div>
+            </nav>
+            <div>
+                <p className='head'>Stay Update with TrendyNews</p>
+            </div>
+            <div className='categoryBtn'>
+                <button onClick={userInput} value="sports">Sports</button>
+            <button onClick={userInput} value="politics">Politics</button>
+            <button onClick={userInput} value="entertainment">Entertainment</button>
+            <button onClick={userInput} value="health">Health</button>
+            <button onClick={userInput} value="fitness">Fitness</button>
+            </div>
+            <div>{newsData ? <Card data={newsData} /> : null}</div>
+        </div>
+    )
+}
+
+export default NewsApp;
